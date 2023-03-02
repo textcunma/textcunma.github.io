0fbcb1ec365f1184979accd4526f76cc14f1a9bc
textcunma <78415309+textcunma@users.noreply.github.com>
2023-03-02 13:10:15 +0900

    update : experience, skills

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,5 +1,5 @@
 <template>
      <footer class="p-4 bg-slate-100 rounded-lg shadow text-right">
-          <span class="text-sm">latest update: 2023/1/8</span>
+          <span class="text-sm">latest update: 2023/3/2</span>
      </footer>
 </template>
